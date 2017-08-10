4531c7809873178d35187080c5547db807165007
Nick <nick@mittudev.com>
2017-08-10 03:09:29 -0400

    Cleanup and add makefiles

--- jni/Tester.java
+++
@@ -1,9 +0,0 @@
-import com.mittudev.ipc.Message;
-
-class Tester{
-  public static void main(String[] args) {
-    Message msg = new Message("Testing the Java JNI API".getBytes());
-    msg.setSubject("Test");
-    msg.destroy();
-  }
-}
